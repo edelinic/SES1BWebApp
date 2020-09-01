2ee1c70e9d4d2177bdaddf578fb354f84700bf3f
cmm <13559048@student.uts.edu.au>
2020-09-01 17:55:53 +1000

    User Story#1: I want to make an account so that I can use the online system

---
+++ SES1B/Models/UserAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SES1B.Models
+{
+    public class UserAccount
+    {
+        [Table("Account")] //Add the name of the table in the database. The error must be something to do with the database methinks
+
+
+        [Key] //Methinks that there's an issue with the SQL components not being set up. 
+        public int Id { get; set; } //setting and getting the account ID
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+
+        
+    }
+
+
+    //public Account()
+      //  {
+            //something here about data annotations and a key
+        //}
+}
---
+++ SES1B/Models/UserAccountContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SES1B.Models
+
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<RestaurantContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserAccount> userAccounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=restaurantdb.cvz3e6ne8iwm.ap-southeast-2.rds.amazonaws.com;database=Restaurant;uid=root;pwd=12345678", x => x.ServerVersion("8.0.17-mysql"));
+            }
+        }
+
+
+
+        //There's a section below where we configure the hell out of the database and say it's all here. 
+
+    }
+}
