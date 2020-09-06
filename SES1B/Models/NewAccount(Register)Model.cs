7b4f3842055041f182fe87276b8360f975e6ca95
ClodaghMM <13559048@student.uts.edu.au>
2020-09-06 11:26:08 +1000

    Added in the attributes to match the user entity in the database

--- SES1B/Models/NewAccount(Register)Model.cs
+++ SES1B/Models/NewAccount(Register)Model.cs
@@ -6,33 +6,37 @@ namespace SES1B.Models
 {
     public class NewAccount : IdentityUser 
 
-    //Gonna have to modify the model so that it takes in works for identity. Please add these in later.
-
-
-
-    { 
-  
-       [Required, EmailAddress, MaxLength(180), Display(Name = "Emaill Address") ]
+    {
+        //These required fields basically mean that the user HAS to enter in this data before they sign up. 
+       [Required, MaxLength(180), Display(Name = "First Name") ]
         public String firstname { get; set;}
+        [Required, MinLength(6), MaxLength(15), DataType(DataType.Custom), Display(Name = "Last Name")]
         public String lastname { get; set; }
+        [Required, MinLength(6), MaxLength(15), DataType(DataType.Custom), Display(Name = "Username")]
+        public String username { get; set;}
+        [Required, MinLength(6), MaxLength(15), DataType(DataType.Password), Display(Name = "Enter Password")]
         public int Password { get; set; }
-       [Required, EmailAddress, MinLength(6), MaxLength(15), DataType(DataType.Password), Display(Name = "Confirm Password")]
+       [Required, MinLength(6), MaxLength(15), DataType(DataType.Password), Display(Name = "Confirm Password")]
         public int confirmPassword { get; set; }
-        [Required, EmailAddress, MinLength(6), MaxLength(15), DataType(DataType.Password), Display(Name = "Password")]
+        [Required, EmailAddress, MinLength(6), MaxLength(15), DataType(DataType.Password), Display(Name = "EmailAddress")]
         public string EmailAddress { get; set; }
-        [Required, Phone, MinLength(8), MaxLengthAttribute(15), Display(Name = "Phone Number")]
+        [Required, Phone, MinLength(8), DataType(DataType.Password), MaxLengthAttribute(15), Display(Name = "Phone Number")]
         public string phoneNumber { get; set; }
+        //Does userID get a seperate required field or is one automatically generated for the user? 
+        public int userID { get; set; }
+        [Required, MinLength(6), MaxLength(8), DataType(DataType.Custom), Display(Name = "Date of Birth")]
+        public int DOB { get; set; }
 
-
-        //[Compare("Password", ErrorMessage = "The password does not match the confirmation password")]
-        public NewAccount(String firstname, String lastname, int password, string emailaddress, string phoneNumber)
+        public NewAccount(String firstname, String lastname, int password, string emailaddress, string phoneNumber, int userID, string username, int dob)
         {
             this.firstname = firstname;
             this.lastname = lastname;
             this.Password = password;
             this.EmailAddress = emailaddress;
-            this.phoneNumber = phoneNumber; 
-
+            this.phoneNumber = phoneNumber;
+            this.DOB = dob; 
+            this.userID = userID;
+            this.username = username;
         }
 
         
--- SES1B/Models/User.cs
+++ SES1B/Models/User.cs
@@ -3,12 +3,15 @@ namespace SES1B.Models
 {
     public class User
     {
-        //Entity class stored in the database
+        //Entity class stored in the database, this class has been modified to match the DB
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+        public int DOB { get; set; }
+        public string phoneNumber { get; set; }
+        public int LoyaltyNumber { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public object Id { get; internal set; }
