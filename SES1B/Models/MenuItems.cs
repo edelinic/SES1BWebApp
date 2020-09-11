82bc2f199fb9ad6f776828b7ea9077023e8f8456
ClodaghMM <13559048@student.uts.edu.au>
2020-09-11 10:10:01 +1000

    Restarted registration to fit tech stack

--- SES1B/Models/MenuItems.cs
+++ SES1B/Models/MenuItems.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-
 namespace SES1B.Models
 {
-    public partial class MenuItems
+    public class MenuItems
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public sbyte? Availability { get; set; }
+        public MenuItems()
+        {
+        }
     }
 }
--- SES1B/Models/User.cs
+++ SES1B/Models/User.cs
@@ -1,24 +1,10 @@
 ﻿using System;
 namespace SES1B.Models
 {
-    public class User
+    public class Registration
     {
-        //Entity class stored in the database, this class has been modified to match the DB
-
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public int DOB { get; set; }
-        public string phoneNumber { get; set; }
-        public int LoyaltyNumber { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public object Id { get; internal set; }
-
-        internal static object SingleOrDefault(Func<object, bool> p)
+        public Registration()
         {
-            throw new NotImplementedException();
         }
     }
 }
