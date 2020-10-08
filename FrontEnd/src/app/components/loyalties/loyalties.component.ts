af5d6c4ef3fbfba8444e8590b55f78db91a88980
JasonI136 <63634201+JasonI136@users.noreply.github.com>
2020-10-08 21:36:11 +1100

    Voucher Generation Complete

--- FrontEnd/src/app/components/loyalties/loyalties.component.ts
+++ FrontEnd/src/app/components/loyalties/loyalties.component.ts
@@ -9,6 +9,36 @@ import { state } from 'src/app/model/state';
 })
 export class LoyaltiesComponent implements OnInit {
   userName: string;
+  code1: string;
+  code2: string;
+  code3: string;
+  possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  lengthOfCode = 15;
+
+  makeRandom(lengthOfCode: number, possible: string) {
+    let text = "";
+    for (let i = 0; i < lengthOfCode; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+      return text;
+  }
+
+  generateCode1()
+  {
+    this.code1 = this.makeRandom(this.lengthOfCode, this.possible);
+    
+  }
+
+  generateCode2()
+  {
+    this.code2 = this.makeRandom(this.lengthOfCode, this.possible);
+    
+  }
+
+  generateCode3()
+  {
+    this.code3 = this.makeRandom(this.lengthOfCode, this.possible);
+  }
 
   constructor(private titleService:Title) {
     this.titleService.setTitle("eDine - Loyalties");
@@ -18,10 +48,20 @@ export class LoyaltiesComponent implements OnInit {
     }
     else{
       this.userName = state.userFirstName + " " + state.userLastName;
-    }
+    } 
    }
 
+      /* To copy Text from Textbox */
+      copyInputMessage(inputElement){
+        inputElement.select();
+        document.execCommand('copy');
+        inputElement.setSelectionRange(0, 0);
+      }
+   
   ngOnInit(): void {
   }
 
+
+  
 }
+
